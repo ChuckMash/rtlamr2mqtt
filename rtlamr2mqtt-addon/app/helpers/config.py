ccc69dcd1a4b9876f20808ed678bed961cf09356
ChuckMash <86080247+ChuckMash@users.noreply.github.com>
2026-08-20 12:16:25 -0700

    Add max_scan_time to general configuration

--- rtlamr2mqtt-addon/app/helpers/config.py
+++ rtlamr2mqtt-addon/app/helpers/config.py
@@ -79,8 +79,8 @@ def load_config(config_path=None):
 
     # General section
     general['listen_mode'] = bool(general.get('listen_mode', False))
-    general['scan_for'] = int(general.get('scan_for', 0))
     general['sleep_for'] = int(general.get('sleep_for', 0))
+    general['max_scan_time'] = int(general.get('max_scan_time', 0))
     general['verbosity'] = str(general.get('verbosity', 'info'))
     general['device_id'] = int(general.get('device_id', 0))
     general['rtltcp_host'] = str(general.get('rtltcp_host', '127.0.0.1:1234'))
